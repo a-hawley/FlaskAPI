f9ad93cca06d119673ad158edfa98004fb383b52
Alec Hawley <alech35@yahoo.com>
2022-05-26 09:55:33 -0500

    Turned text returns into JSON, added multiply function

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
-
+from distutils.log import debug
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return '<p> Hello World!</p>'
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        json_ = request.get_json()
+        return jsonify({'POSTED: ': json_}), 201
+    else:
+        return jsonify({'about': "Hello world"})
 
 
-if __name__ == '__main__':
-    hello_world()
+@app.route('/multi/<int:num>', methods=['GET'])
+def get_multiply(num):
+    return jsonify({'result': num*num})
+
+
+app.run(debug=True)
